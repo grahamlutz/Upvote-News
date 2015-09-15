d73a898d89d2cc82ed7218729f4b0c7da32e3854
Graham Lutz <graham.lutz@gmail.com>
2015-09-15 17:06:40 -0400

    STYLES!

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('upvoteNews', []);
 
 app.controller('MainCtrl', ['$scope', function($scope) {
 	$scope.test = "Hello World!";
 	$scope.posts = [
 		{title:'post 1', upvotes: 0},
 		{title:'post 2', upvotes: 0},
-		{title:'post 3', upvotes: 1},
+		{title:'post 3', upvotes: 0},
 		{title:'post 4', upvotes: 0},
-		{title:'post 5', upvotes: 3},
+		{title:'post 5', upvotes: 0},
 		{title:'post 6', upvotes: 0}
 	];
 	$scope.addPost = function() {
 		if(!$scope.title || $scope.title === '') {return;}
-		$scope.posts.push({title: $scope.title, upvotes: 0});
+		$scope.posts.push({
+			title: $scope.title,
+			link: $scope.link, 
+			upvotes: 0
+		});
 		$scope.title = '';
+		$scope.link = '';
 	};
+	$scope.incrementUpvotes = function(post) {
+		post.upvotes += 1;
+	}
 }]);
