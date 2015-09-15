fb4c935290c55fce0c55fb18ef3be217daae14cd
Graham Lutz <graham.lutz@gmail.com>
2015-09-15 18:57:05 -0400

    Implement 'PostCtrl' controller and add comment functionality

--- app.js
+++ app.js
@@ -21,10 +21,35 @@ app.config([
 				url: '/home',
 				templateUrl: '/home.html',
 				controller: 'MainCtrl'
+			})
+			.state('posts', {
+				url: '/posts/{id}',
+				templateUrl: '/posts.html',
+  				controller: 'PostsCtrl'
 			});
 		$urlRouterProvider.otherwise('home');
 	}
 ]);
+
+app.controller('PostsCtrl', [
+	'$scope',
+	'$stateParams',
+	'posts',
+	function($scope, $stateParams, posts) {
+		$scope.addComment = function(){
+		  if($scope.body === '') { return; }
+		  $scope.post.comments.push({
+		    body: $scope.body,
+		    author: 'user',
+		    upvotes: 0
+		  });
+		  $scope.body = '';
+		};
+		$scope.post = posts.posts[$stateParams.id];
+		$scope.incrementUpvotes = function(comment) {
+			comment.upvotes += 1;
+		}
+}]);
 
 app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
 	$scope.test = "Hello World!";
@@ -34,7 +59,11 @@ app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
 		$scope.posts.push({
 			title: $scope.title,
 			link: $scope.link, 
-			upvotes: 0
+			upvotes: 0,
+			comments: [
+    			{author: 'Joe', body: 'Cool post!', upvotes: 0},
+    			{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+  			]
 		});
 		$scope.title = '';
 		$scope.link = '';
