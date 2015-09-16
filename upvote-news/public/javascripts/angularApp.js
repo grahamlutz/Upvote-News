24655c43d041e91e4557da405113a9302077af20
Graham Lutz <graham.lutz@gmail.com>
2015-09-16 11:05:03 -0400

    Import angular project to Node

---
+++ upvote-news/public/javascripts/angularApp.js
@@ -0,0 +1,76 @@
+(function() {
+var app = angular.module('upvoteNews', ['ui.router', 'ngRoute']);
+
+app.factory('posts', [function(){
+	var o = {
+    posts: [
+    	{title: 'LinkedIn', link: "http://linkedin.com/in/grahamlutz", upvotes: 0},
+    	{title: 'Google', link: "http://www.google.com", upvotes: 0},
+    	{title: 'Facebook', link: "http://www.facebook.com", upvotes: 0}
+    ]
+  	};
+  	return o;
+}]);
+
+app.config([
+	'$stateProvider', 
+	'$urlRouterProvider', 
+	function($stateProvider, $urlRouterProvider){
+		$stateProvider
+			.state('home',{
+				url: '/home',
+				templateUrl: '/home.html',
+				controller: 'MainCtrl'
+			})
+			.state('posts', {
+				url: '/posts/{id}',
+				templateUrl: '/posts.html',
+  				controller: 'PostsCtrl'
+			});
+		$urlRouterProvider.otherwise('home');
+	}
+]);
+
+app.controller('PostsCtrl', [
+	'$scope',
+	'$stateParams',
+	'posts',
+	function($scope, $stateParams, posts) {
+		$scope.addComment = function(){
+		  if($scope.body === '') { return; }
+		  $scope.post.comments.push({
+		    body: $scope.body,
+		    author: 'user',
+		    upvotes: 0
+		  });
+		  $scope.body = '';
+		};
+		$scope.post = posts.posts[$stateParams.id];
+		$scope.incrementUpvotes = function(comment) {
+			comment.upvotes += 1;
+		}
+}]);
+
+app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
+	$scope.test = "Hello World!";
+	$scope.posts = posts.posts;
+	$scope.addPost = function() {
+		if(!$scope.title || $scope.title === '') {return;}
+		$scope.posts.push({
+			title: $scope.title,
+			link: $scope.link, 
+			upvotes: 0,
+			comments: [
+    			{author: 'Joe', body: 'Cool post!', upvotes: 0},
+    			{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+  			]
+		});
+		$scope.title = '';
+		$scope.link = '';
+	};
+	$scope.incrementUpvotes = function(post) {
+		post.upvotes += 1;
+	}
+}]);
+
+})();
